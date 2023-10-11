f28644c2bfefb0b3120a30dfb4e8deca30975181
lmoranallen <lmoranallen@gmail.com>
2023-10-11 22:08:30 +0100

    Working scroll animation that resets on refresh - dot expansion text active but buggy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 
+const characterList = ['dev', 'chef', 'gym', 'weeb', 'lee'];
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -19,12 +20,13 @@ const LeadingTitle = styled.h1`
   font-size: 100px;
   font-family: 'Montserrat', sans-serif;
   margin-top: 24px;
+  margin-right: 24px;
 `
 
 const VerticalBox = styled.div`
-  width: 240px;
-  height: 120px;
-  overflow-y: scroll;
+  width: auto;
+  height: 160px;
+  overflow-y: hidden;
 
   // chrome, safari and opera
   ::webkit-scrollbar {
@@ -34,18 +36,46 @@ const VerticalBox = styled.div`
   // IE, Edge and Firefox
   -ms-overflow-style: none;
   scrollbar-width: none;
+`
 
+const scrollAnimation = keyframes`
+  0% { -moz-transform: translateY(0); }
+  100% {-moz-transform: translateY(${characterList.length*-152}px); }
+`;
+
+
+const expandDotAnimation = keyframes`
+  0% { font-size: 100px; }
+  100% { font-size:10000px; }
 `
 
 const VerticalScrollText = styled(LeadingTitle)`
-  width: auto;
-  
+  // animation-name: ${scrollAnimation};
+  // animation-duration: 5s;
+  // animation-fill-mode: forwards;
+`;
+
+const LeadingDot = styled.span`
+  z-index: 100;
+  animation-name: ${expandDotAnimation};
+  animation-duration: 5s;
+  animation-fill-mode: forwards;
+`
+
+const VerticalScrollElement = styled.div`
 `
 
 const App = () => {
   return (
     <Container>
-       <TextBox><VerticalBox><VerticalScrollText>dev chef gym lee</VerticalScrollText></VerticalBox><LeadingTitle> .chay </LeadingTitle></TextBox>
+       <TextBox>
+        <VerticalBox>
+          <VerticalScrollElement >
+            {characterList.map( elem => <VerticalScrollText>{elem}</VerticalScrollText>)} 
+          </VerticalScrollElement>
+        </VerticalBox>
+        <LeadingTitle> <LeadingDot>.</LeadingDot>chay </LeadingTitle>
+       </TextBox>
     </Container>
    
   );
