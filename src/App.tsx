b669ab8616233ffbf3eab38a9ea6f16d54715bb0
lmoranallen <lmoranallen@gmail.com>
2023-10-14 00:45:03 +0100

    Successful chaining on dot animation to slots. Confetti now in progress

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styled, { keyframes } from 'styled-components';
 
 
 const characterList = ['dev', 'chef', 'gym', 'weeb', 'lee'];
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -45,22 +46,61 @@ const scrollAnimation = keyframes`
 
 
 const expandDotAnimation = keyframes`
-  0% { font-size: 100px; }
-  100% { font-size:10000px; }
+  0% { transform: scale(1); }
+  100% { transform: scale(1000); }
+`;
+
+const loadPageAnimation = keyframes`
+  0% { opacity: 1; }
+  100% { background-color: white; z-index: -1; }
+`;
+
+const resetDot = keyframes`
+  0% { background-color: white; }
+  100% { background-color: black; transform: scale(1);}
 `
 
 const VerticalScrollText = styled(LeadingTitle)`
-  // animation-name: ${scrollAnimation};
-  // animation-duration: 5s;
-  // animation-fill-mode: forwards;
-`;
-
-const LeadingDot = styled.span`
-  z-index: 100;
-  animation-name: ${expandDotAnimation};
+  animation-name: ${scrollAnimation};
   animation-duration: 5s;
   animation-fill-mode: forwards;
-`
+`;
+
+const FullDot = styled.div`
+  z-index: 100;
+  width: 1%;
+  height: 1%;
+  border-radius: 50%;
+  background-color: black;
+  top: 51%;
+  left: 50%;
+  position: fixed;
+  animation-name: ${expandDotAnimation} , ${loadPageAnimation};
+  animation-delay: 6s;
+  animation-duration: 5s;
+  animation-fill-mode: forwards;
+`;
+
+const ConfettiBlock = styled.div`
+  width: 6%;
+  height: 45%;
+  
+  background-color: black;
+  &:nth-child(3n + 1) {
+    rotate: -50deg;
+    background-color: green;
+    margin-right: 120px;
+  }
+  &:nth-child(3n + 2) {
+    background-color: blue;
+  }
+  &:nth-child(3n) {
+    rotate: 50deg;
+    background-color: red;
+    margin-left: 120px;
+  }
+  
+`;
 
 const VerticalScrollElement = styled.div`
 `
@@ -69,12 +109,15 @@ const App = () => {
   return (
     <Container>
        <TextBox>
+       {/* <ConfettiBlock/>
+       <ConfettiBlock/>
+       <ConfettiBlock/> */}
         <VerticalBox>
           <VerticalScrollElement >
             {characterList.map( elem => <VerticalScrollText>{elem}</VerticalScrollText>)} 
           </VerticalScrollElement>
         </VerticalBox>
-        <LeadingTitle> <LeadingDot>.</LeadingDot>chay </LeadingTitle>
+        <FullDot/><LeadingTitle> chay </LeadingTitle>
        </TextBox>
     </Container>
    
